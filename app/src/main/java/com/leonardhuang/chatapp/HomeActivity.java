9d61179f746417b96401deb8e1f254039604e9a6
Léonard Huang <56548224+LeonardHuang99@users.noreply.github.com>
2021-11-17 15:17:06 +0100

    Initial

---
+++ app/src/main/java/com/leonardhuang/chatapp/HomeActivity.java
@@ -0,0 +1,29 @@
+package com.leonardhuang.chatapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class HomeActivity extends AppCompatActivity {
+
+    private TextView tw_signup;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+        tw_signup = findViewById(R.id.textview_signup);
+
+    }
+
+
+    public void signupListeners(View view) {
+        Intent intent = new Intent(this,SignUpActivity.class);
+        startActivity(intent);
+    }
+}
---
+++ app/src/main/java/com/leonardhuang/chatapp/SignUpActivity.java
@@ -0,0 +1,27 @@
+package com.leonardhuang.chatapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class SignUpActivity extends AppCompatActivity {
+
+    private TextView tw_signin;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+
+        tw_signin = findViewById(R.id.textview_have_account);
+    }
+
+    public void signinListeners(View view) {
+
+        Intent intent = new Intent(this,HomeActivity.class);
+        startActivity(intent);
+    }
+}
