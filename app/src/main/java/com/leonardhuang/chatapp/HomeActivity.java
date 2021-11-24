7d3d30745a125c93985669619405bc9295a86f78
RohailSHABBIR10543 <92733202+RohailSHABBIR10543@users.noreply.github.com>
2021-11-24 01:09:38 +0100

    Firebase firestore

--- app/src/main/java/com/leonardhuang/chatapp/HomeActivity.java
+++ app/src/main/java/com/leonardhuang/chatapp/HomeActivity.java
@@ -7,6 +7,11 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -25,5 +30,23 @@ public class HomeActivity extends AppCompatActivity {
     public void signupListeners(View view) {
         Intent intent = new Intent(this,SignUpActivity.class);
         startActivity(intent);
+        //binding.buttonSignIn.setOnClickListener(v -> addDataToFirestore());
     }
+
+    //a supprimer, pour test
+    private void addDataToFirestore(){
+        FirebaseFirestore database = FirebaseFirestore.getInstance();
+        HashMap<String, Object> data = new HashMap<>();
+        data.put("first_name", "Leonard");
+        data.put("last_name", "Le BG");
+        database.collection("users")
+                .add(data)
+                .addOnSuccessListener(documentReference -> {
+                    Toast.makeText(getApplicationContext(), "Data Inserted", Toast.LENGTH_SHORT).show();
+                })
+                .addOnFailureListener(exception -> {
+                    Toast.makeText(getApplicationContext(), exception.getMessage(), Toast.LENGTH_SHORT).show();
+                });
+    }
+
 }
--- app/src/main/java/com/leonardhuang/chatapp/firebase/MessagingService.java
+++ app/src/main/java/com/leonardhuang/chatapp/firebase/MessagingService.java
@@ -3,11 +3,17 @@ package com.leonardhuang.chatapp.firebase;
 import androidx.annotation.NonNull;
 
 import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
 
 public class MessagingService extends FirebaseMessagingService {
 
     @Override
-    public void onNewToken(@NonNull String s) {
-        super.onNewToken(s);
+    public void onNewToken(@NonNull String token) {
+        super.onNewToken(token);
+    }
+
+    @Override
+    public void onMessageReceived(@NonNull RemoteMessage remoteMessage) {
+        super.onMessageReceived(remoteMessage);
     }
 }
