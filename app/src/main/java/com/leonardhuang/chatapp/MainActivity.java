29bca3b1040c58b1bd64a47580fe694b00cf375c
Léonard Huang <56548224+LeonardHuang99@users.noreply.github.com>
2021-11-24 09:57:54 +0100

    COmmit2

--- app/src/main/java/com/leonardhuang/chatapp/MainActivity.java
+++ app/src/main/java/com/leonardhuang/chatapp/MainActivity.java
@@ -12,7 +12,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    //salut
+    //Rohail leohgfdfghjk
+    //aaaaaaah
     //zzzzzz
 
 }
