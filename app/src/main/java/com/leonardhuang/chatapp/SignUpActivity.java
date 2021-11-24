953dc26f3145949a140d0b16732409554d3f3009
Léonard Huang <56548224+LeonardHuang99@users.noreply.github.com>
2021-11-24 12:03:16 +0100

    Commit2

--- app/src/main/java/com/leonardhuang/chatapp/SignUpActivity.java
+++ app/src/main/java/com/leonardhuang/chatapp/SignUpActivity.java
@@ -1,10 +1,16 @@
 package com.leonardhuang.chatapp;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContract;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.util.Base64;
 import android.view.View;
 import android.widget.Button;
@@ -13,7 +19,14 @@ import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.leonardhuang.chatapp.utilities.Constants;
+import com.makeramen.roundedimageview.RoundedImageView;
+
 import java.io.ByteArrayOutputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.util.HashMap;
 
 public class SignUpActivity extends AppCompatActivity {
 
@@ -26,12 +39,14 @@ public class SignUpActivity extends AppCompatActivity {
     private EditText input_email;
     private EditText input_password;
     private EditText input_confirm_password;
+    private RoundedImageView image_profile;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+        image_profile = findViewById(R.id.image_profile);
         tw_signin = findViewById(R.id.textview_have_account);
         button_signup = findViewById(R.id.button_signup);
         input_fname = findViewById(R.id.input_fname);
@@ -44,6 +59,7 @@ public class SignUpActivity extends AppCompatActivity {
     public void signInListeners(View view) {
         Intent intent = new Intent(this,HomeActivity.class);
         startActivity(intent);
+
     }
 
     public void signUpListeners(View view){
@@ -54,15 +70,36 @@ public class SignUpActivity extends AppCompatActivity {
         });
     }
 
+    public void imageProfileListeners(View view) {
+        Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+        pickImage.launch(intent);
+    }
+
     private void showToast(String message){
         Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
     }
 
     private void signUp(){
+        loading(true);
+        FirebaseFirestore database = FirebaseFirestore.getInstance();
+        HashMap<String,Object> user = new HashMap<>();
+        user.put(Constants.KEY_FIRST_NAME,input_fname.getText().toString());
+        user.put(Constants.KEY_LAST_NAME,input_lname.getText().toString());
+        user.put(Constants.KEY_EMAIL,input_email.getText().toString());
+        user.put(Constants.KEY_PASSWORD,input_password.getText().toString());
+        user.put(Constants.KEY_IMAGE,encodedImage);
+        database.collection(Constants.KEY_COLLECTION_USERS)
+                .add(user)
+                .addOnSuccessListener(documentReference -> {
 
+                })
+                .addOnFailureListener(exception ->{
+
+                } );
     }
 
-    private String encodedImage(Bitmap bitmap){
+    private String encodeImage(Bitmap bitmap){
         int previewWidth = 150;
         int previewHeight = bitmap.getHeight() * previewWidth / bitmap.getWidth();
         Bitmap previewBitmap = Bitmap.createScaledBitmap(bitmap, previewWidth, previewHeight, false);
@@ -71,6 +108,26 @@ public class SignUpActivity extends AppCompatActivity {
         byte[] bytes = byteArrayOutputStream.toByteArray();
         return Base64.encodeToString(bytes, Base64.DEFAULT);
     }
+
+    private final ActivityResultLauncher<Intent> pickImage = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+            result -> {
+                if(result.getResultCode() == RESULT_OK){
+                    if(result.getData() != null){
+                        Uri imageUri = result.getData().getData();
+                        try {
+                            InputStream inputStream = getContentResolver().openInputStream(imageUri);
+                            Bitmap bitmap = BitmapFactory.decodeStream(inputStream);
+                            image_profile.setImageBitmap(bitmap);
+                            encodedImage = encodeImage(bitmap);
+                        }
+                        catch (FileNotFoundException e){
+                            e.printStackTrace();
+                        }
+                    }
+                }
+            }
+    )
 
     private Boolean isValidSignUpDetails(){
         if(encodedImage == null){
