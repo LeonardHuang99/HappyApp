2e59ce25eebc2211aeb316724ab7f269eab1bed0
RohailSHABBIR10543 <92733202+RohailSHABBIR10543@users.noreply.github.com>
2021-12-08 11:13:50 +0100

    tokenEdit

--- app/src/main/java/com/leonardhuang/chatapp/MainActivity.java
+++ app/src/main/java/com/leonardhuang/chatapp/MainActivity.java
@@ -38,6 +38,7 @@ public class MainActivity extends AppCompatActivity {
         loadUserDetails();
         getToken();
         setListeners();
+        //LEo
     }
 
     private void setListeners(){
