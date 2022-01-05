7467cd44977b91656583bca113ece9cc1442dfef
RohailSHABBIR10543 <92733202+RohailSHABBIR10543@users.noreply.github.com>
2022-01-05 10:40:55 +0100

    layoutsend

---
+++ app/src/main/java/com/leonardhuang/chatapp/activity/ChatActivity.java
@@ -0,0 +1,16 @@
+package com.leonardhuang.chatapp.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.leonardhuang.chatapp.R;
+
+public class ChatActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chat);
+    }
+}
